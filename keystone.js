a1c2c2df9ec41643b90d0ebed9d01d7a75b8847b
Steen Larsen <steen@cloudbastion.com>
2018-02-13 21:02:15 +0100

    Prepare for use in openshift.
    - Added openshift_template file which can be used for openshift deployment
    - Modified keystone.js to read certain information about Mongodb setup from Openshift environment

--- keystone.js
+++ keystone.js
@@ -1,5 +1,29 @@
 var keystone = require('keystone');
 
+
+    //  Steen Larsen : Openshift adaption
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+	mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+	mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+	mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+	mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+	mongoUser = process.env[mongoServiceName + '_USER'];
+
+    if (mongoHost && mongoPort && mongoDatabase) {
+	mongoURLLabel = mongoURL = 'mongodb://';
+	if (mongoUser && mongoPassword) {
+	    mongoURL += mongoUser + ':' + mongoPassword + '@';
+	}
+	mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+    }
+}
+// console.log("Hello : " + mongoURL);
+// SL END
+
 keystone.init({
 
 	'name': 'Keystone Demo',
@@ -9,11 +33,17 @@ keystone.init({
 	'less': 'public',
 	'static': 'public',
 
+        /* 
+	   * Steen Larsen, wanted to add the next two lines for Openshift but similar code is already present in ./node_modules/keystone/index.js
+	   * 'port' : process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+	   * 'ip' :   process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+	   */
+         'mongo': mongoURL, // SL: adapted
+
 	'views': 'templates/views',
 	'view engine': 'pug',
 
-	'auto update': true,
-	'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/keystone-demo',
+        'auto update': true,
 	'cloudinary config': 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
 
 	'session': true,
